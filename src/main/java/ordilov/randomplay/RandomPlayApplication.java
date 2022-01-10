1e622edf20a54f6d3a8c62bd7cc236c8570862e1
ordilov <ordilov@gmail.com>
2022-01-10 18:27:45 +0900

    Initial commit

---
+++ src/main/java/ordilov/randomplay/RandomPlayApplication.java
@@ -0,0 +1,13 @@
+package ordilov.randomplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomPlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ordilov/randomplay/RandomPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package ordilov.randomplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
