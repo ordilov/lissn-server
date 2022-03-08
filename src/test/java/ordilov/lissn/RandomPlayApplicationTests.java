82947fa5802ce236d833706304571584de24b6bf
ordilov <ordilov@gmail.com>
2022-03-08 23:11:52 +0900

    add config

--- src/test/java/ordilov/lissn/RandomPlayApplicationTests.java
+++ src/test/java/ordilov/lissn/RandomPlayApplicationTests.java
@@ -1,12 +1,13 @@
 package ordilov.lissn;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RandomPlayApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+  @Test
+  void contextLoads() {
+  }
 
 }
