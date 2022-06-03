4a041f68427b533f1ef345203567bf49f66abebb
ordilov <ordilov@gmail.com>
2022-06-03 09:47:47 +0900

    rename main app

--- src/main/java/ordilov/lissn/LissnApplication.java
+++ src/main/java/ordilov/lissn/LissnApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RandomPlayApplication {
+public class LissnApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RandomPlayApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(LissnApplication.class, args);
+  }
 
 }
